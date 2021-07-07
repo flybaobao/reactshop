17c5730ea1087260d73c5649c555b85e3745a69f
songda <38792057+flybaobao@users.noreply.github.com>
2021-07-08 01:04:37 +0800

    示例

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 // import Css from "../../assets/css/common/common.css";
-// import "../../assets/css/common/common.css";
+import Css from "../../assets/css/home/homePage.css";
 export default class IndexComponent extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
   render() {
-    return <div class="app">我是app</div>;
+    return (
+      <div class="app">
+        <p>我是app</p>
+        <div class={Css["home"]}>我是内部文字</div>
+      </div>
+    );
     // return <div class={Css["app"]}>我是app</div>;
   }
 }
