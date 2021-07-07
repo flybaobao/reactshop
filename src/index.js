15c5794f48a0abde0f033729b35a0f3ad1ac0d95
songda <38792057+flybaobao@users.noreply.github.com>
2021-07-08 01:29:11 +0800

    默认不支持 ie
    配置babel-polyfill  url-search-params-polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/*eslint-disable*/
+import "babel-polyfill";
+import "url-search-params-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./router";
@@ -10,7 +13,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
